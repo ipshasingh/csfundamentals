821d002f68883d5087fab8f6ab3994a50e4d901a
Ipsha Singh <ipsha.singh28@gmail.com>
2026-09-06 23:43:55 +0530

    generate relation set logic complete

--- discretemaths/hasse.py
+++ discretemaths/hasse.py
@@ -18,6 +18,42 @@ def generate_tree(rule):
     print(ast.dump(tree,indent=2))
     return tree
 
+def evaluate(node, variables):
+    if isinstance(node, ast.Constant):
+        return node.value
+
+    if isinstance(node, ast.Name):
+        return variables[node.id]
+
+    if isinstance(node, ast.BinOp):
+        left = evaluate(node.left, variables)
+        right = evaluate(node.right, variables)
+        if isinstance(node.op, ast.Add):
+            return left + right
+        elif isinstance(node.op, ast.Sub):
+            return left - right
+        elif isinstance(node.op, ast.Mult):
+            return left * right
+        elif isinstance(node.op, ast.Div):
+            return left / right    
+
+    if isinstance(node, ast.Compare):
+        left = evaluate(node.left, variables)
+        right = evaluate(node.comparators[0], variables)
+        if isinstance(node.ops[0], ast.Eq):
+            return left == right
+        elif isinstance(node.ops[0], ast.NotEq):
+            return left != right
+        elif isinstance(node.ops[0], ast.Lt):
+            return left < right
+        elif isinstance(node.ops[0], ast.LtE):
+            return left <= right
+        elif isinstance(node.ops[0], ast.Gt):
+            return left > right
+        elif isinstance(node.ops[0], ast.GtE):
+            return left >= right 
+        else:
+            raise ValueError("Unsupported comparison operator") 
 
 def take_set():
     S=set()
@@ -44,12 +80,14 @@ CP=set()
 
 
 R=set()
+rule = enter_relation()
+tree = generate_tree(rule)
 for i in S:
     for j in S:
         x=(i,j)
         print("x:",x)
         CP.add(x)  
-        if relation(str(i),str(j),op_use):   
+        if evaluate(tree.body, {"a" : i, "b" : j}):   
             print("relation satisfied by:",x)   
             R.add(x)   
 
