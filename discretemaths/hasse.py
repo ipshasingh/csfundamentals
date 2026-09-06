270b8a1c1df1c5fd8bedc194c3c0324ba234e59d
Ipsha Singh <ipsha.singh28@gmail.com>
2026-09-06 20:50:23 +0530

    making relation set from moderated output

---
+++ discretemaths/hasse.py
@@ -0,0 +1,48 @@
+def show_op():
+    op=['<','>','>=','==','!=']
+    print(op)
+    op_use=input("Enter the operater for relation: ")
+    if op_use=="=":
+        op_use = "=="
+    return op_use
+
+def take_set():
+    S=set()
+    n=int(input("Enter the number of elements in the set: "))
+    for i in range(n):
+        x=int(input("Enter the element: "))
+        S.add(x)
+    return S
+
+def relation(a,b,x):
+    
+    if eval(a+x+b):
+        return True
+    else:
+        return False
+    
+S=take_set()
+
+print("Set is :",S)
+
+
+CP=set()
+
+op_use=show_op()
+print("Relation def is : a",op_use,"b")
+
+
+R=set()
+for i in S:
+    for j in S:
+        x=(i,j)
+        print("x:",x)
+        CP.add(x)  
+        if relation(str(i),str(j),op_use):   
+            print("relation satisfied by:",x)   
+            R.add(x)   
+
+print("Cartesian Product:",CP)
+print("Relation:",R)
+
+
---
+++ discretemaths/try.py
@@ -0,0 +1,8 @@
+a="1"
+b="2"
+x="<"
+print(eval(a+x+b))
+print(a<x>b)
+a="4"
+b="5"
+print(eval(b+x+a))
